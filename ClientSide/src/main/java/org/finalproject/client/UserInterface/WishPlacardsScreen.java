70a1e2d32cf60a761adc6206ed42d0247df6b420
hjt-ghasemi <hjt.ghasmi@gmail.com>
2023-01-14 13:36:53 +0330

    wish placard screen updated

--- ClientSide/src/main/java/org/finalproject/client/UserInterface/WishPlacardsScreen.java
+++ ClientSide/src/main/java/org/finalproject/client/UserInterface/WishPlacardsScreen.java
@@ -2,10 +2,12 @@ package org.finalproject.client.UserInterface;
 
 import org.finalproject.DataObject.SalePlacard;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class WishPlacardsScreen extends PlacardsScreen{
+public class WishPlacardsScreen extends UIScreen{
+    private List<SalePlacard> placards;
     public WishPlacardsScreen(Scanner scanner) {
         super(scanner);
     }
@@ -13,10 +15,33 @@ public class WishPlacardsScreen extends PlacardsScreen{
     @Override
     void printGuideMessage() {
         UIUtils.header("My Wish Placards Page");
-        List<SalePlacard> placardList = getTestPlacards();
+        fetchPlacards();
         int i = 1;
-        for (SalePlacard placard : placardList) {
+        for (SalePlacard placard : placards) {
             UIUtils.placardTemplate(i++, placard, true);
+        }
+
+        UIUtils.secondary("You can select a placard for more details. (type 'back' to back to the home menu)");
+    }
+
+    void fetchPlacards(){
+        //TODO related http request to fetch wish list of the user
+        placards = new ArrayList<>();
+    }
+
+    @Override
+    void processInput(){
+        String input = notEmptyInput(scanner.nextLine()) ;
+        if(input.equals("back")){
+            new HomeMenuScreen(scanner).guide().process();
+            return;
+        }
+        try{
+            int index = Integer.parseInt(input) - 1;
+            new PlacardScreen(scanner, placards.get(index), this).guide().process();
+            return;
+        }catch(NumberFormatException ex){
+            restartWithError(input+" is not a meaningful command in this context.");
         }
     }
 
